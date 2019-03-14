d1e173da1018a8e0b57ce3297569183c2290d2c8
Valdir Coxev <seti1@zornimat.com.br>
2019-03-14 15:14:13 -0400

    Aula: Configuração do Saga e Axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,21 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
 import '../configs/reactotron';
+import sagas from './sagas';
 
-export default (process.env.NODE_ENV === 'development'
-  ? createStore(reducers, compose(console.tron.createEnhancer()))
-  : createStore(reducers));
+const middlewares = [];
+const sagaMiddleware = createSagaMiddleware();
+middlewares.push(sagaMiddleware);
+
+const store = process.env.NODE_ENV === 'development'
+  ? createStore(
+    reducers,
+    compose(
+      console.tron.createEnhancer(),
+      applyMiddleware(...middlewares),
+    ),
+  )
+  : createStore(reducers);
+sagaMiddleware.run(sagas);
+export default store;
